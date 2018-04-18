f5c153412e523ebe2a8a41a576ccd3f9a9ffe7cf
Eli Baum <elimbaum@gmail.com>
2018-04-17 20:43:47 -0400

    finished tokenizer

--- tokenize.py
+++ tokenize.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+#
+# Takahashi Tokenizer
+# by Eli Baum
 
 # TODO:
 # 89r2 paragraph issue
-# weirdo characters
-# column breaks
+# column breaks \
+#	34r probably fine
+#	39r definitely fine
+#	89r2 looks like different paragraph?
 
 from collections import Counter
 import re
@@ -19,38 +24,55 @@ with open("text16e6.evt", 'r', encoding="latin_1") as file:
 
 		transcription = m.group(1)
 
+		# ignore entire line if it has a {&NNN} or {&.} code
+		if re.search(r'\{&(\d|\.)+\}', transcription):
+			continue
+
 		# remove extraneous chracters ! and %
 		s = transcription.translate(str.maketrans("","","!%"))
 
 		# delete all end of line {comments} (between one and three observed)
 		# ...with optional line terminator
-		s = re.sub(r'([-=]?\{[^\{\}]+?\}){1,3}[-=]?$', "", s)
+		# allow 0 occurences to remove end-of-line markers (- or =)
+		s = re.sub(r'([-=]?\{[^\{\}]+?\}){0,3}[-=]?\s*$', "", s)
 
 		# delete start of line {comments} (single or double)
 		s = re.sub(r'^(\{[^\{\}]+?\}){1,2}', "", s)
 
 		# these tags are word breaks
-		s = re.sub(r'[-=]\{(plant|figure|gap|root|hole|spray|gal|sync gap|diagram)\}', ".", s)
+		# breaks = [r'plant', r'figure', r'gap', r'root', r'hole', r'spray',
+		# 		  r'gal', r'sync gap', r'diagram', r'star', r'blot\??', r'stem',
+		# 		  r'stitched slit', r'stream', r'top of diagram', r'wide gap',
+		# 		  r'\|\|', r'fold', r'crease']
+		# s = re.sub(r'[-=]\{(' + r'|'.join(breaks) + r')\}', ".", s)
+
+		# simplification: tags preceeded by -= are word breaks
+		s = re.sub(r'[-=]\{[^\{\}]+?\}', '.', s)
 
 		# these tags are nulls
-		s = re.sub(r'\{(fold|crease)\}', "", s)
+		# plant is a null in one case where it is just {plant}
+		# otherwise (above) it is a word break
+		# s = re.sub(r'\{(fold|crease|blot|&\w.?|plant)\}', "", s)
+		# simplification: remaining tags in curly brackets
+		s = re.sub(r'\{[^\{\}]+?\}', '', s)
 
-		# if "{" in s:
-		print(s)
+		# special case .{\} is still a word break
+		s = re.sub(r'\.\{\\\}', ".", s)
 
-		# split on word boundaries or illustrations (/ other annotations)
-		#words = re.split(r'\.|-?\{.+\}', line)
+		# split on word boundaries
+		words = s.split(".")
 
-		#print(words)
+		# print(words)
 		
-		#wordCounter.update(w.translate(str.maketrans("",""," -=!%")) for w in filter(None, words))
+		wordCounter.update(words)
 
-#print(len(wordCounter))
 
-# s = 0
 
-# for i, w in enumerate(wordCounter.most_common()):
-# 	print(w)
-# 	s += w[1]
+s = 0
 
-#print(s)
+for i, w in enumerate(wordCounter.most_common()):
+	print(w)
+	s += w[1]
+
+print(len(wordCounter))
+print(s)
